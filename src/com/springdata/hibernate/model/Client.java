b0196fbefdcf682e795250845f73789eeba312e1
mostafa <mosta.abdo250@gmail.com>
2022-02-02 00:49:54 +0200

    create client model

---
+++ src/com/springdata/hibernate/model/Client.java
@@ -0,0 +1,32 @@
+package com.springdata.hibernate.model;
+
+public class Client {
+	private long id;
+	private String fullName;
+	private int age;
+	private String address;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
